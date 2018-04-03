7d950f49112417da004e9ad1289b598833d0c405
Keith Bloom <keith.bloom@weareadaptive.com>
2018-04-03 18:03:14 +0100

    Added types

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for show-version 
+// Project: https://github.com/keithbloom/show-version.git  
+// Definitions by: Keith Bloom 
+
+declare function showVersion() : string;
+
+export = showVersion;
