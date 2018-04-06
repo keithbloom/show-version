9ce97092d18db8e3569e6a077e3440eb755a361a
Keith Bloom <keith.bloom@gmail.com>
2018-04-06 11:58:52 +0100

    Changed the message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export function show() {
-    console.log(`show-version version 2`);
+    console.log(`show-version version 3`);
 }
 
 export function getVersionMessage() {
-    return 'show-version version 2';
+    return 'show-version version 3';
 }
