e7c1dc40e9546a704f8c15a7fd95c6def1d18567
Keith Bloom <keith.bloom@gmail.com>
2018-04-06 11:34:24 +0100

    Updates to typings

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,9 @@
 // Project: https://github.com/keithbloom/show-version.git  
 // Definitions by: Keith Bloom 
 
-export as namespace ShowVersion
-export function show() : string;
+export as namespace ShowVersion;
+export = ShowVersion;
+
+declare namespace ShowVersion {
+    export function show() : string;
+}
